079f7eb5b3b620796556653d4397659883aa1e0d
pkakkar521 <114657672+pkakkar521@users.noreply.github.com>
2023-10-11 17:55:31 +0530

    front

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
-} from 'react-router-dom';
-import { useAuthContext } from './hooks/useAuthContext';
-
-// pages & components
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Navbar from './components/Navbar';
+import logo from './logo.svg';
+import './App.css';
+import AllRoutes from './routes/AllRoutes';
+import { Navbar } from './components/Homepage/Navbar';
 
 function App() {
-	const { user } = useAuthContext();
+  return (
+    <div className="App">
+      <Navbar />
+      <AllRoutes />
 
-	return (
-		<div className='App'>
-			<Router>
-				<Navbar />
-				<div className='pages container'>
-					<Routes>
-						<Route
-							path='/'
-							element={user ? <Home /> : <Navigate to='/login' />}
-						/>
-						<Route
-							path='/login'
-							element={!user ? <Login /> : <Navigate to='/' />}
-						/>
-						<Route
-							path='/signup'
-							element={!user ? <Signup /> : <Navigate to='/' />}
-						/>
-					</Routes>
-				</div>
-			</Router>
-		</div>
-	);
+    </div>
+  );
 }
 
 export default App;
---
+++ src/constants/config.js
@@ -0,0 +1,1 @@
+export const BASE_URL="https://white-coyote-sock.cyclic.app"
